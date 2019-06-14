8e6bade9ef13b5a6be73c158328662005deca6f0
Spencer Rosenfeld <spencerrosenfeld@ufl.edu>
2019-06-14 15:07:22 -0400

    Add project files.

---
+++ ChessGame/main.cpp
@@ -0,0 +1,59 @@
+#include <SDL.h>
+#include <iostream>
+#include  <thread>
+#include <chrono>
+
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 600; 
+
+SDL_Window * window = NULL;
+SDL_Renderer * renderer = NULL;
+SDL_Surface * surface = NULL; 
+
+bool init();
+
+int main(int argc, char * args [] )
+{
+	init();
+
+	std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+
+	return 0;
+}
+
+bool init()
+{
+	bool success = true; 
+
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+		success = false;
+	}
+	else
+	{
+		if (!SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "1"))
+		{
+		}
+
+		window = SDL_CreateWindow("Chess Game", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+		
+		if (window == NULL)
+		{
+			success = false;
+		}
+		else
+		{
+			renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+			if(renderer == NULL)
+			{
+				success = false;
+			}
+			else
+			{
+				SDL_SetRenderDrawColor(renderer, 0xff, 0xff, 0xff, 0xff);
+			}
+		}
+	}
+	return success;
+}
+
