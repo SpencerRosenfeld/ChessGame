b9c4a5846b1f9abcd52f1a02c847dfdd500fa02e
Marvin <lionking89@ufl.edu>
2019-06-15 15:48:22 -0400

    Made some progress towards adding a apwn image to screen

--- ChessGame/main.cpp
+++ ChessGame/main.cpp
@@ -7,11 +7,46 @@
 const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 600; 
 
+class LTexture {
+public:
+	//Initialize variables
+	LTexture();
+
+	//Deallocates memory
+	~LTexture();
+
+	//Load image at a specified path
+	bool loadFromFile(std::string path);
+
+	//Deallocates texture
+	void free();
+
+	//Render texture at a given point
+	void render(int x, int y);
+
+	//Get image dimensions
+	int getWidth();
+	int getHeight();
+	void setWidth(int);
+	void setHeight(int);
+
+private:
+	SDL_Texture * texture;
+
+	int width;
+	int height;
+};
+
 SDL_Window * window = NULL;
 SDL_Renderer * renderer = NULL;
 SDL_Surface * surface = NULL; 
 
 bool init();
+bool loadMedia();
+void close();
+
+LTexture pieceTexture;
+LTexture boardTexture;
 
 int main(int argc, char * args [] )
 {
@@ -20,6 +55,13 @@ int main(int argc, char * args [] )
 	std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 
 	return 0;
+}
+
+LTexture::LTexture() {
+	//Initialize
+	texture = NULL;
+	width = 0;
+	height = 0;
 }
 
 bool init()
@@ -52,9 +94,26 @@ bool init()
 			else
 			{
 				SDL_SetRenderDrawColor(renderer, 0xff, 0xff, 0xff, 0xff);
+
+				int imgFlags = IMG_INIT_PNG;
+				if (!(IMG_Init(imgFlags) & imgFlags)) {
+					success = false;
+				}
 			}
 		}
 	}
 	return success;
 }
 
+void close() {
+	boardTexture.free();
+	pieceTexture.free();
+
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	window = NULL;
+	renderer = NULL;
+
+	IMG_Quit();
+	SDL_Quit();
+}
