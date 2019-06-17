23a5d8a6c5ac9951892f74e41fe7de706a0a3c16
Marvin <lionking89@ufl.edu>
2019-06-17 14:09:00 -0400

    Made some changes to ChessPiece.cpp.

--- ChessGame/ChessPiece.cpp
+++ ChessGame/ChessPiece.cpp
@@ -3,17 +3,11 @@
 
 ChessPiece::ChessPiece()
 {
-	//team 1 = white, team 0 = black
-	//Under the chess rule, the white team goes first
-	team = 1;
 
-	x = 0;
-	y = 0;
-	isCaptured = false;
 }
 
 
 ChessPiece::~ChessPiece()
 {
-	free();
+	
 }
