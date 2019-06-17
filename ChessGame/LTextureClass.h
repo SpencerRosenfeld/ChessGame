3dd634e0540f6885050a273acd19ffc7c2d9a82b
Marvin <lionking89@ufl.edu>
2019-06-17 14:49:08 -0400

    Added the LTextureClass header and definition files to create functions and variables associated with the class.

---
+++ ChessGame/LTextureClass.h
@@ -0,0 +1,39 @@
+#pragma once
+#include <SDL.h>
+#include <SDL_image.h>
+#include <string>
+#include <iostream>
+
+class LTextureClass
+{
+public:
+	//Initialize variables
+	LTextureClass();
+
+	//Deallocates memory
+	~LTextureClass();
+
+	//Load image at a specified path
+	bool loadFromFile(std::string path);
+
+	//Deallocates texture
+	void free();
+
+	//Render texture at a given point
+	void render(int x, int y);
+
+	//Get image dimensions
+	int getWidth();
+	int getHeight();
+	void setWidth(int);
+	void setHeight(int);
+
+private:
+	SDL_Texture * texture;
+	SDL_Renderer * renderer;
+	SDL_Window * window;
+	SDL_Surface * surface;
+	int width;
+	int height;
+};
+
