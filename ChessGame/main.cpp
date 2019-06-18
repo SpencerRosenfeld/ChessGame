adf43ee17713e09fdf20f942c839d080e4740cf3
Marvin <lionking89@ufl.edu>
2019-06-18 15:11:48 -0400

    Removed all the definitions and declarations already added to the LTextureClass from main.cpp.

--- ChessGame/main.cpp
+++ ChessGame/main.cpp
@@ -7,53 +7,17 @@
 const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 600; 
 
-class LTexture {
-public:
-	//Initialize variables
-	LTexture();
-
-	//Deallocates memory
-	~LTexture();
-
-	//Load image at a specified path
-	bool loadFromFile(std::string path);
-
-	//Deallocates texture
-	void free();
-
-	//Render texture at a given point
-	void render(int x, int y);
-
-	//Get image dimensions
-	int getWidth();
-	int getHeight();
-	void setWidth(int);
-	void setHeight(int);
-
-private:
-	SDL_Texture * texture;
-
-	int width;
-	int height;
-};
-
 SDL_Window * window = NULL;
 SDL_Renderer * renderer = NULL;
 SDL_Surface * surface = NULL; 
 
 bool init();
-bool loadMedia();
 void close();
 
-LTexture pieceTexture;
-LTexture boardTexture;
 
 int main(int argc, char * args [] )
 {
 	init();
-
-//	std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-	loadMedia();
 	bool quit = false;
 
 	SDL_Event ev;
@@ -68,12 +32,9 @@ int main(int argc, char * args [] )
 			}
 
 		}
-//		SDL_SetRenderDrawColor(renderer, 0, 0, 0xFF, 0xFF);
 		SDL_RenderClear(renderer);
 
-		boardTexture.render(0, 0);
 
-		pieceTexture.render(50, 50);
 
 		SDL_RenderPresent(renderer);
 	}
@@ -85,80 +46,6 @@ int main(int argc, char * args [] )
 	return 0;
 }
 
-LTexture::LTexture() {
-	//Initialize
-	texture = NULL;
-	width = 0;
-	height = 0;
-}
-
-LTexture::~LTexture() {
-	//Deallocate
-	free();
-}
-
-bool LTexture::loadFromFile(std::string path) {
-	//Remove pre-existing texture
-	free();
-
-	SDL_Texture * newTexture = NULL;
-
-	SDL_Surface * loadedSurface = IMG_Load(path.c_str());
-	if (loadedSurface == NULL) {
-		IMG_GetError();
-	}
-	else {
-		//Color key image
-		SDL_SetColorKey(loadedSurface, SDL_TRUE, SDL_MapRGB(loadedSurface->format, 0, 0xFF, 0xFF));
-
-		//Create texture from surface pixels
-		newTexture = SDL_CreateTextureFromSurface(renderer, loadedSurface);
-		if (newTexture == NULL) {
-			SDL_GetError();
-		}
-		else {
-			//Get image dimensions
-			width = loadedSurface->w;
-			height = loadedSurface->h;
-		}
-
-		//Remove old loaded surface
-		SDL_FreeSurface(loadedSurface);
-	}
-
-	texture = newTexture;
-	return texture != NULL;
-}
-
-void LTexture::free() {
-	if (texture != NULL) {
-		SDL_DestroyTexture(texture);
-		texture = NULL;
-		width = 0;
-		height = 0;
-	}
-}
-
-void LTexture::render(int x, int y) {
-	SDL_Rect renderQuad = { x, y, width, height };
-	SDL_RenderCopy(renderer, texture, NULL, &renderQuad);
-}
-
-int LTexture::getWidth() {
-	return width;
-}
-
-int LTexture::getHeight() {
-	return height;
-}
-
-void LTexture::setWidth(int w) {
-	width = w;
-}
-
-void LTexture::setHeight(int h) {
-	height = h;
-}
 
 bool init()
 {
@@ -201,19 +88,8 @@ bool init()
 	return success;
 }
 
-bool loadMedia() {
-	bool success = true;
-
-	if (!pieceTexture.loadFromFile("b_bishop_2x_ns.bmp")) {
-		success = false;
-	}
-	return success;
-}
 
 void close() {
-	boardTexture.free();
-	pieceTexture.free();
-
 	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(window);
 	window = NULL;
